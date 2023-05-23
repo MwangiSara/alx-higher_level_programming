ce4a5d5f30b5e35791bd37dbdf5da98de29e7fb1
sarah Mwangi <114574064+MwangiSara@users.noreply.github.com>
2023-05-23 16:12:52 +0300

    Create 3-say_my_name.py

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This is the "Say My Name" module.
+
+The Say My Name module takes one required parameter and one optional.
+It prints "My name is (first) (last)" where (first) and (last) are the args.
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Print My name is (first) (last) if given, else print error.
+    """
