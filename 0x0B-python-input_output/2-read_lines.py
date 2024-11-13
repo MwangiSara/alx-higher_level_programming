5912ef87d21f68d71b8e3c6add66afb24cf177b5
sarah Mwangi <114574064+MwangiSara@users.noreply.github.com>
2024-11-13 07:46:53 -0800

    Update 2-read_lines.py

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-def read_lines(filename="", nb_lines=0):
-    with open(filename, encoding="utf-8") as fd:
-        if (nb_lines == 0):
-            print(fd.read(), end="")
-        else:
-            for line in fd:
-                if (nb_lines > 0):
-                    print(line, end="")
-                    nb_lines -= 1
+"""defines function to return dictionary description for JSON serialization"""
+
+
+def class_to_json(obj):
+    """returns dictionary description for JSON serialization"""
+    return obj.__dict__
