9347a2865126402bce0ff869829bc37079d28d5d
sarah Mwangi <114574064+MwangiSara@users.noreply.github.com>
2024-11-13 05:42:49 -0800

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-# 4-hidden_discovery.py
-if __name__ == "__main__":
-    """Print all names defined by hidden_4 module."""
+if __name__ == '__main__':
     import hidden_4
-
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+    names_list = dir(hidden_4)
+    for name in names_list:
+        if name[0] != '_':
+            print("{}".format(name))
