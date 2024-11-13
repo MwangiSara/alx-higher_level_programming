76f68d3fe868453d7ccd1c0237ac9bf6cd9914da
sarah Mwangi <114574064+MwangiSara@users.noreply.github.com>
2024-11-13 07:39:37 -0800

    Update 101-add_attribute.py

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-def add_attribute(obj, a, v):
-    res = getattr(obj, "__doc__", None)
-    if res is None:
-        setattr(obj, a, v)
-    else:
+"""defines function to check if you can add attribute"""
+
+
+def add_attribute(obj, name, value):
+    """checks if can add atrribute"""
+    if '__dict__' not in dir(obj):
         raise TypeError("can't add new attribute")
+    else:
+        setattr(obj, name, value)
